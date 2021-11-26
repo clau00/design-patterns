8713e0fc9996484122b9b97649d39d6eb31e5c92
claudiu <danbalint_88@yahoo.com>
2021-11-26 12:31:12 +0200

    CB: design-patterns - Principles and Strategies: Single Responsibility Principle, 1st part

---
+++ PrinciplesAndStrategiesOfDesign/src/com/company/singleResponsibilityPrinciple/Employee.java
@@ -0,0 +1,30 @@
+package com.company.singleResponsibilityPrinciple;
+
+import java.util.Date;
+
+public class Employee {
+
+	// this class breaks the single responsibility principle because:
+	// the logic to determine the promotion must be done by HR department, not by the Employee class
+	// the logic to determine the income must be done by Finance department, not by the Employee class
+	// if one of the HR or Finance policy changes, the Employee class suffers changes too
+
+	private String employeeId;
+	private String name;
+	private String address;
+	private Date dateOfJoining;
+
+	public boolean isPromotionDueThisYear() {
+		// promotion logic
+
+		return false;
+	}
+
+	public double calcIncomeTaxForCurrentYear() {
+		// income tax logic
+
+		return 0L;
+	}
+
+	// getters and setters for all member variables
+}
