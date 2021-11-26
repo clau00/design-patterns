bfd794c78e47c9d3259eb15478727335dc88cb02
claudiu <danbalint_88@yahoo.com>
2021-11-26 12:09:49 +0200

    CB: design-patterns - Principles and Strategies: Delegation Principles

---
+++ PrinciplesAndStrategiesOfDesign/src/com/company/delegationPrinciples/PrintTester.java
@@ -0,0 +1,30 @@
+package com.company.delegationPrinciples;
+
+class RealPrinter {
+	// the "delegate"
+
+	void print() {
+		System.out.println("The Delegate (RealPrinter)");
+	}
+}
+
+class Printer {
+	// the "delegator"
+
+	// create the delegate
+	RealPrinter realPrinter = new RealPrinter();
+
+	// use the delegate
+	void print() {
+		realPrinter.print();
+	}
+}
+
+public class PrintTester {
+	// to the outside world it looks like the Printer actually prints
+
+	public static void main(String[] args) {
+		Printer printer = new Printer();
+		printer.print();
+	}
+}
