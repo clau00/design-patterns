d55184db80fac457e90fc35c1a79ac369120ff5e
claudiu <danbalint_88@yahoo.com>
2021-11-27 11:25:46 +0200

    CB: design-patterns - Principles and Strategies: Dependency Injection Principle, END part - solution, via constructor or setter injection

--- PrinciplesAndStrategiesOfDesign/src/com/company/dependencyInjectionPrinciple/Client.java
+++ PrinciplesAndStrategiesOfDesign/src/com/company/dependencyInjectionPrinciple/Client.java
@@ -1,23 +1,47 @@
 package com.company.dependencyInjectionPrinciple;
 
+interface Service {
+	void write();
+}
+
+class ServiceA implements Service {
+	@Override
+	public void write() {
+		System.out.println("Hello World");
+	}
+}
+
 public class Client {
 
 	// internal reference to the service used by this client
-	private ExampleService service;
+	private Service service;
 
 	Client() {
 		// use a specific implementation in the constructor instead of using dependency injection
-		service = new ExampleService();
+		service = new ServiceA();
+	}
+
+	Client(Service service) {
+		// injects via the constructor
+		this.service = service;
+	}
+
+	public void setService(Service service) {
+		// injects via the setter
+		this.service = service;
 	}
 
 	// method within this client to use the service
-	public String greet() {
-		return "Hello " + service.getName();
+	public void greet() {
+		service.write();
+	}
+
+	public static void main(String[] args) {
+		Service service = new ServiceA(); // the injector
+		Client client = new Client(service); // injects via the constructor
+		client.setService(service); // injects via setter
+		client.greet();
 	}
 }
 
-class ExampleService {
-	public String getName() {
-		return "the Name";
-	}
-}
+
