bbc664f05f384149801a7dabe4bf1149ada05290
claudiu <danbalint_88@yahoo.com>
2021-11-28 10:22:05 +0200

    CB: design patterns: Factory Method - abstract creator

---
+++ creational_design_patterns/src/com/company/factorymethod/abstractcreator/Client.java
@@ -0,0 +1,21 @@
+package com.company.factorymethod.abstractcreator;
+
+public class Client {
+
+	public static void main(String[] args) {
+
+		// get an object o Circle and call its draw method
+		Shape circle = new CircleFactory().getShape();
+		circle.draw();
+
+		// get an object o Rectangle and call its draw method
+		Shape rectangle = new RectangleFactory().getShape();
+		rectangle.draw();
+
+		// get an object o Circle and call its draw method
+		Shape square = new SquareFactory().getShape();
+		square.draw();
+
+	}
+
+}
