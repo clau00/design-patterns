daf05b8d0d0e35ebff3e7d474520e004ff3cd552
claudiu <danbalint_88@yahoo.com>
2021-11-28 10:28:40 +0200

    CB: design patterns: Factory Method - concrete creator

---
+++ creational_design_patterns/src/com/company/factorymethod/concretecreator/ShapeFactory.java
@@ -0,0 +1,21 @@
+package com.company.factorymethod.concretecreator;
+
+import java.awt.*;
+
+public class ShapeFactory {
+
+	// use getShape method to get object of type shape
+	public Shape getShape(String shapeType) {
+		if(shapeType == null) {
+			return null;
+		} else if(shapeType.equalsIgnoreCase("CIRCLE")) {
+			return new Circle();
+		}else if(shapeType.equalsIgnoreCase("SQUARE")) {
+			return new Square();
+		}else if(shapeType.equalsIgnoreCase("RECTANGLE")) {
+			return new Rectangle();
+		}
+
+		return null;
+	}
+}
