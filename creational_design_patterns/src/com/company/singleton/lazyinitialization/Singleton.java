c74dc2b6cafe26db8cc7ac0bebdf95417fe789a1
claudiu <danbalint_88@yahoo.com>
2021-11-29 11:31:55 +0200

    CB: design patterns: Singleton - lazy initialization

---
+++ creational_design_patterns/src/com/company/singleton/lazyinitialization/Singleton.java
@@ -0,0 +1,33 @@
+package com.company.singleton.lazyinitialization;
+
+public class Singleton {
+
+	// lazy implementation means we will not create it unless we will need it (calling getInstance())
+	// PROBLEM(when the singleton has state - other fields): is not thread safe!!!
+
+	// the private static reference to the one and only instance
+	private static Singleton uniqueInstance = null;
+
+	// an instance attribute
+	private int data = 0;
+
+	// the constructor, must be defined as private
+	private Singleton() {}
+
+	public static Singleton getInstance() {
+		if(uniqueInstance == null) {
+			uniqueInstance = new Singleton();
+		}
+
+		return uniqueInstance;
+	}
+
+	public void setData(int myData) {
+		data = myData;
+	}
+
+	public int getData() {
+		return data;
+	}
+
+}
